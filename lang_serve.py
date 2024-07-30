67cae385f841cc2c64144535d2e18d9d9cbbcde6
M-yoshimura-ML <masadrst@gmail.com>
2024-07-30 11:51:03 +0800

    LangServe

---
+++ lang_serve.py
@@ -0,0 +1,42 @@
+# pip install fastapi uvicorn
+# pip install langserve
+from dotenv import load_dotenv
+from fastapi import FastAPI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
+from langserve import add_routes
+import os
+
+load_dotenv()
+
+groq_api_key = os.environ.get('GROQ_API_KEY')
+model = ChatGroq(model="Gemma2-9b-It", groq_api_key=groq_api_key)
+
+# 1. Create prompt template
+system_template = "Template the following into {language}:"
+prompt_template = ChatPromptTemplate.from_messages([
+    ("system", system_template),
+    ("user", "{text}")
+])
+
+parser = StrOutputParser()
+
+# create chain
+chain = prompt_template | model | parser
+
+# App definition
+app = FastAPI(title="LangChain",
+              version="1.0",
+              description="A simple API server using LangChain runnable interfaces")
+
+add_routes(
+    app,
+    chain,
+    path="/chain"
+)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8000)
+
